8a02b9208fb8ab7c5082c17a07715a6585c67109
margarcuae <margarcuae@gmail.com>
2020-09-06 15:34:48 -0500

    todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+    div.todo.container5
+        
+        i(class='far fa-square todo__check' v-bind:class="{nomostrar:isChecked}")
+        i.fas.fa-check-square.todo__check(v-bind:class="{nomostrar:!isChecked}")
+        div.todo__item
+            p.todo__item__texto {{texto}}
+            i(class="fas fa-trash todo__item__basura")
+</template>
+
+<script>
+export default {
+	name: "Todo",
+	props: {
+		texto: String,
+		isChecked: Boolean,
+	},
+};
+</script>
+
+<style lang="sass">
+@import '@/assets/sass/main'
+
+.todo
+    display: flex
+    // text-align: center
+    align-items: center
+    margin-bottom: 5px
+    p
+        margin: 0
+    &__check
+        color: $color-primario
+    &__item
+        margin-left: 10px
+        width: 100%
+        display: grid
+        grid-template-columns: 80% 20%
+        align-items: center
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
+        border-radius: 10px
+        padding: 6px
+
+        &__basura
+            justify-self: end
+
+
+
+.fa-trash
+    color: white
+    background-color: $color-secundario
+    padding: 6px
+    border-radius: 14px
+</style>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-  div.inicio
-    Barra
-    Buscador
-    br
-    h1 Bienvenida {{usuario.nombre}} as
-    button.boton New to-do
+    div.inicio
+        Barra
+        Buscador
+        br
+        h1 Bienvenida {{usuario.nombre}} as
+        button.boton New to-do
+        br
+        br
+        Todo(texto='No procrastinar' isChecked=true)
+        Todo(texto='No procrastinar' isChecked=false)
+        
+
   
     
 </template>
@@ -12,6 +18,7 @@
 <script>
 import Barra from "@/components/Barra.vue";
 import Buscador from "@/components/Buscador.vue";
+import Todo from "@/components/Todo.vue";
 import { mapState } from "vuex";
 
 export default {
@@ -19,6 +26,7 @@ export default {
 	components: {
 		Barra,
 		Buscador,
+		Todo,
 	},
 	methods: {},
 	computed: {
